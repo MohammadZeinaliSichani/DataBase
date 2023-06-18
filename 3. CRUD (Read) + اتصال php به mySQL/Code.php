b410370891060d38820f353349f8757f9cb164d0
Mohammad Zeinali Sichani <108570779+MohammadZeinaliSichani@users.noreply.github.com>
2023-06-19 00:41:00 +0330

    افزودن و حذف نرم و گرفتن لیست همه محصولات

---
+++ 3. CRUD (Read) + اتصال php به mySQL/Code.php
@@ -0,0 +1,14 @@
+افزودن محصول جدید :
+
+INSERT INTO Products (ID,Name,Code,Price,Entity)
+VALUES ("1","T-Shirt","158","200000","5")
+
+حذف (نرم) محصول :
+
+UPDATE products
+SET Status = 'DeActive'
+WHERE Id = 1
+
+گرفتن لیست همه محصولات :
+
+SELECT * Products
